242ca4efe715c73849a138dc23a764478a9ae7eb
Andriy Bilous <andrij.bilous@gmail.com>
2015-08-01 17:29:09 +0300

    dynamic page students_list created

--- students/views.py
+++ students/views.py
@@ -3,7 +3,25 @@ from django.http import HttpResponse
 
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u'Віталій',
+         'last_name': u'Подоба',
+         'ticket': 235,
+         'image': 'img/picture1.png'},
+        {'id': 2,
+         'first_name': u'Дмитро',
+         'last_name': u'Іванів',
+         'ticket': 2135,
+         'image': 'img/picture2.png'},
+        {'id': 3,
+         'first_name': u'Ярослав',
+         'last_name': u'Вархол',
+         'ticket': 2178,
+         'image': 'img/picture3.png'}
+    )
+    return render(request, 'students/students_list.html',
+                  {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Add Student Form</h1>')
