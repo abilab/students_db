1adaef0e80516cd137e64e58b8e3c5a6568bc256
Andriy Bilous <andrij.bilous@gmail.com>
2015-08-02 21:52:52 +0300

    groups_list.html dynamic created

--- students/views.py
+++ students/views.py
@@ -23,23 +23,44 @@ def students_list(request):
     return render(request, 'students/students_list.html',
                   {'students': students})
 
+
 def students_add(request):
     return HttpResponse('<h1>Add Student Form</h1>')
+
 
 def students_edit(request, sid):
     return HttpResponse('<h1>Edit Student %s</h1>' % sid)
 
+
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete Student %s</h1>' % sid)
 
+
 def groups_list(request):
-    return HttpResponse('<h1>Groups List</h1>')
+    groups = (
+        {'id': 1,
+         'group_name': 'МтМ-21',
+         'group_leader': 'Ячмонов Олег',
+         'group_leader_id': 21},
+        {'id': 2,
+         'group_name': 'МтМ-22',
+         'group_leader': 'Подоба Віталій',
+         'group_leader_id': 22},
+        {'id': 3,
+         'group_name': 'МтМ-23',
+         'group_leader': 'Іванов Андрій',
+         'group_leader_id': 23}
+    )
+    return render(request, 'students/group_list.html', {'groups': groups})
+
 
 def groups_add(request):
     return HttpResponse('<h1>Add Group Form</h1>')
 
+
 def groups_edit(request, gid):
     return HttpResponse('<h1>Edit Group %s' % gid)
+
 
 def groups_delete(request, gid):
     return HttpResponse('<h1>Delete Group %s' % gid)
