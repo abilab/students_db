c1d1c8abbef7d7b1d75b997396171cdc690d5675
Andriy Bilous <andrij.bilous@gmail.com>
2015-08-22 18:16:43 +0300

    context_processors have been added

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -64,6 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request',
+                'studentsdb.context_processors.students_proc',
             ],
         },
     },
@@ -101,3 +103,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PORTAL_URL = 'http://localhost:8000'
